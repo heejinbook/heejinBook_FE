9b467f4b4e72eab63f7a6284f5c7bddcb29333db
heelws <162127@naver.com>
2023-10-10 04:53:52 +0900

    feat : Toastify

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import { router } from './pages/routes';
+import { StyledToastContainer } from './components/common/Toastify/Toastify';
 
 function App() {
   return (
     <RecoilRoot>
       <RouterProvider router={router} />
+      <StyledToastContainer limit={1} />
     </RecoilRoot>
   );
 }
