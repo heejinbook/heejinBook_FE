0a74615498b639aff8d27664f587a7f8e5c6261c
heelws <162127@naver.com>
2023-12-01 19:19:19 +0900

    feat : my library css

--- src/components/MyLibrary/LibraryReview/LibraryReviewItems/LibraryReviewItems.styles.tsx
+++ src/components/MyLibrary/LibraryReview/LibraryReviewItems/LibraryReviewItems.styles.tsx
@@ -1,5 +1,13 @@
 import styled from '@emotion/styled';
 
+export const ReviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0 15px;
+`;
+
 export const BookImage = styled.img`
   width: 120px;
   height: 180px;
@@ -16,7 +24,7 @@ export const BookTitle = styled.div`
   justify-content: center;
   font-size: 16px;
   font-weight: bold;
-  margin: 16px 0 2px 0;
+  margin: 10px 0 2px 0;
 `;
 
 export const BookAuthor = styled.div`
@@ -26,21 +34,22 @@ export const BookAuthor = styled.div`
   align-items: center;
   justify-content: center;
   font-size: 14px;
-  margin-bottom: 9px;
+  margin-top: 10px;
 `;
 
 export const ReviewPhraseContainer = styled.div`
   width: 300px;
   height: auto;
   display: flex;
-  align-items: start;
+  align-items: center;
   justify-content: center;
   text-align: center;
+  margin-top: 10px;
   p {
     display: flex;
     align-items: start;
     padding: 0;
-    margin: 0 20px;
+    margin: 0;
     font-size: 24px;
     color: #503f15;
   }
@@ -55,5 +64,5 @@ export const ReviewPhrase = styled.div`
   justify-content: center;
   text-align: center;
   font-size: 14px;
-  padding-top: 3px;
+  margin: 0 5px;
 `;
