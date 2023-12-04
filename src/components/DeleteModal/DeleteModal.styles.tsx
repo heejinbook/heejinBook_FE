273ba0176e29f865fbae7f8e82f1688a7d2a7127
heelws <162127@naver.com>
2023-12-05 04:18:10 +0900

    feat : my library book delete modal

---
+++ src/components/DeleteModal/DeleteModal.styles.tsx
@@ -0,0 +1,77 @@
+import styled from '@emotion/styled';
+
+type Modal = {
+  modalOpen: boolean;
+};
+
+export const DeleteModalContainer = styled.div<Modal>`
+  background-color: ${({ modalOpen }) => (modalOpen ? 'rgba(0, 0, 0, 0.2)' : 'none')};
+  z-index: ${({ modalOpen }) => (modalOpen ? '999' : '-1')};
+  display: ${({ modalOpen }) => (modalOpen ? 'block' : 'none')};
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
+
+export const DeleteModal = styled.div<Modal>`
+  width: 400px;
+  height: auto;
+  display: ${({ modalOpen }) => (modalOpen ? 'block' : 'none')};
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fffdf6;
+  border-radius: 10px;
+  box-sizing: border-box;
+  p {
+    font-size: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 80px;
+    margin: 0;
+    padding: 0;
+    color: #503f15;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 0;
+`;
+
+export const CancelBtn = styled.button`
+  cursor: pointer;
+  width: 100%;
+  height: 50px;
+  padding: 10px 40px;
+  border: none;
+  border-radius: 0 0 0 10px;
+  background-color: #d5cfbf;
+  color: #503f15;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const DeleteBtn = styled.button`
+  cursor: pointer;
+  width: 100%;
+  height: 50px;
+  padding: 10px 40px;
+  border: none;
+  border-radius: 0 0 10px 0;
+  background-color: #fcf2cc;
+  color: #503f15;
+  font-size: 16px;
+  font-weight: bold;
+`;
--- src/components/MyLibrary/LibraryList/LibraryList.tsx
+++ src/components/MyLibrary/LibraryList/LibraryList.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import IconX from '../../../assets/svg/circleX.svg';
 import { useGetLibraryBook } from '../../../querys/bookQuery';
 import { useDeleteBook } from '../../../querys/bookMutation';
+import { useState } from 'react';
+import { DeleteModal } from '../../DeleteModal/DeleteModal';
 
 export type LibraryBookType = {
   bookId: number;
@@ -12,11 +14,18 @@ export type LibraryBookType = {
 };
 
 export function LibraryList() {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [selected, setSelected] = useState<number>(0);
+
   const navigate = useNavigate();
 
   const { data: libraryBook } = useGetLibraryBook();
 
   const { deleteBookMutate } = useDeleteBook();
+
+  const modalCloseHandler = () => {
+    setModalOpen(false);
+  };
 
   return (
     libraryBook && (
@@ -26,8 +35,22 @@ export function LibraryList() {
           {libraryBook
             .map((book) => (
               <S.LibraryListItems key={book.bookId}>
+                <DeleteModal
+                  modalOpen={modalOpen}
+                  modalClose={modalCloseHandler}
+                  clickDelete={deleteBookMutate}
+                  selected={selected}
+                  phrase={'선택한 책을 책장에서 삭제하시겠습니까?'}
+                />
+
                 <div style={{ position: 'relative' }}>
-                  <S.LibraryDelete src={IconX} onClick={() => deleteBookMutate(book.bookId)} />
+                  <S.LibraryDelete
+                    src={IconX}
+                    onClick={() => {
+                      setModalOpen(true);
+                      setSelected(book.bookId);
+                    }}
+                  />
                   <div
                     onClick={() => {
                       navigate(`/main/books/${book.bookId}`);
