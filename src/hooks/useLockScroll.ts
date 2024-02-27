df77a10b7450062487b7eb7c629362dd961d31b1
heewls <162127@naver.com>
2024-02-27 17:33:25 +0900

    feat : modal open 시 scroll lock

---
+++ src/hooks/useLockScroll.ts
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+
+export function useLockScroll(modalOpen: boolean) {
+  useEffect(() => {
+    if (modalOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [modalOpen]);
+}
